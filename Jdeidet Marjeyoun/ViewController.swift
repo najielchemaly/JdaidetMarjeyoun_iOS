15104c4a655b7522da7c4263741e23f1000cc546
NajiElChemaly <naji.chemaly@intouchmena.com>
2017-09-11 09:35:32 +0300

    Initial Commit

---
+++ Jdeidet Marjeyoun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Jdeidet Marjeyoun
+//
+//  Created by MR.CHEMALY on 9/11/17.
+//  Copyright © 2017 marjeyoun-municipality. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
